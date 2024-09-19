e5c362efd551cc36d9e97a29415b8e67fb893264
Kᴏɴsᴛᴀɴᴛɪɴ Sʜɪʟᴏᴠ <shilovk@gmail.com>
2024-09-19 15:18:43 +0300

    feat(fonts): use fonts

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,11 @@
-import './App.css';
+import styles from './app.module.css';
+
 import Header from "../header/header";
 import React from "react";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.app}>
       <Header />
     </div>
   );
