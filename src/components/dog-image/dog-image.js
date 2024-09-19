35bb9f489fe28d5b88b1233cf22f5f1762167aea
Kᴏɴsᴛᴀɴᴛɪɴ Sʜɪʟᴏᴠ <shilovk@gmail.com>
2024-09-19 15:18:43 +0300

    feat(css): use folders and module styles

--- src/components/dog-image/dog-image.js
+++ src/components/dog-image/dog-image.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import dogImagePath from '../../images/dog.png';
+import dogImage from "./dog-image.css";
 
 class DogImage extends React.Component {
     render() {
         return (
-            <img src={dogImagePath} alt='Грустная собачка' />
+            <div className={dogImage.card}>
+                <img
+                    className={dogImage.image}
+                    src={this.props.image}
+                    alt="Грустная собачка"
+                />
+                <h2 className={dogImage.title}>{this.props.name}</h2>
+                <span className={dogImage.description}>{this.props.description}</span>
+            </div>
         );
     }
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,21 @@
 import logo from "../../images/logo.svg";
 import React from "react";
 import DogImage from "../dog-image/dog-image";
+import styles from "./header.module.css";
+import dogImagePath from '../../images/dog.png';
 
 class Header extends React.Component {
     render() {
         return (
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
+            <header className={styles.header}>
+                <img src={logo} className={styles.logo} alt="logo"/>
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
-                <DogImage />
+                <DogImage image={dogImagePath} name="Rubymine" description="cool" />
 
                 <a
-                    className="App-link"
+                    className={styles.link}
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
