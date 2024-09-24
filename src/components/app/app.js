50f6d57a3dc80ad16b4653c20d6c951c441fd929
Kᴏɴsᴛᴀɴᴛɪɴ Sʜɪʟᴏᴠ <shilovk@gmail.com>
2024-09-24 19:07:33 +0300

    feat(modal): use portal

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,47 @@
-import styles from './app.module.css';
-
-import Header from "../header/header";
 import React from "react";
+import styles from "./app.module.css";
+import Header from "../header/header";
 import VideoPlayer from "../video-player/video-player";
 import AutoPlay from "../video-player/auto-play/auto-play";
+import Modal from "../modal/modal";
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <VideoPlayer />
-      <AutoPlay />
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal() {
+    this.setState({ visible: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ visible: false });
+  }
+
+  render() {
+    return (
+      <>
+        <VideoPlayer />
+        <AutoPlay />
+        <div className={styles.app}>
+          <button onClick={this.handleOpenModal}>Открыть модальное окно</button>
+          {this.state.visible && (
+            <Modal header="Внимание!" onClose={this.handleCloseModal}>
+              <p>Спасибо за внимание!</p>
+              <p>Открывай меня, если станет скучно :)</p>
+            </Modal>
+          )}
+        </div>
+        <Header />
+      </>
+    );
+  }
 }
 
 export default App;
