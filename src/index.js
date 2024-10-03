9e67eeff25be51aae5eefaf38f89b980787604b3
Kᴏɴsᴛᴀɴᴛɪɴ Sʜɪʟᴏᴠ <shilovk@gmail.com>
2024-10-03 23:06:40 +0300

    feat(redux): add thunk

--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import { rootReducer } from "./services/reducers";
 import { compose, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
     : compose;
 
-const enhancer = composeEnhancers(
-  applyMiddleware()
-);
+const enhancer = composeEnhancers(applyMiddleware(thunk));
 
 const store = createStore(rootReducer, enhancer);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
