30d5777f3e25d4670a8a678a2b01bdcec8e23e2c
Kᴏɴsᴛᴀɴᴛɪɴ Sʜɪʟᴏᴠ <shilovk@gmail.com>
2024-10-03 18:07:42 +0300

    feat(redux): add support devtool

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/app';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from "redux";
 import {Provider} from "react-redux";
 import { rootReducer } from "./services/reducers";
+import { compose, createStore, applyMiddleware } from 'redux';
 
-const store = createStore(rootReducer);
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware()
+);
+
+const store = createStore(rootReducer, enhancer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
